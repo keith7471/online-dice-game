7fbba3f4e47011e35ff1c0b9ef1aede333df80d8
keith7471 <93572477+keith7471@users.noreply.github.com>
2023-05-06 15:14:17 +0530

    Added changes to the random number concept
    The dice 2 was not changing values

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var image_selectr_1= document.querySelectorAll("img")[0];
 image_selectr_1.setAttribute("src" ,random_image_1);
 
 
-var randomnumber_2 = Math.floor(Math.random()) * 6 +1;
+var randomnumber_2 = Math.floor(Math.random() * 6) +1;
 var random_image_2 = "images/" + "dice" + randomnumber_2 + ".png";
 var image_2 = document.querySelectorAll("img")[1];
 image_2.setAttribute("src", random_image_2);
